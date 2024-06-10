37e14152ca82a6785a76d6fa943d74a93c2db585
Ankit Ydv <ankityadav11241@gmail.com>
2024-06-10 13:17:08 +0530

    duplicate code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { FaGithub, FaTimes } from "react-icons/fa";
