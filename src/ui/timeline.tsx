1baacd962e7014fe46ef7fdffceeec16f4bedc19
Ankit Ydv <ankityadav11241@gmail.com>
2025-04-01 22:14:41 +0530

    fix: unused imports

--- src/ui/timeline.tsx
+++ src/ui/timeline.tsx
@@ -1,6 +1,4 @@
-"use client";
-import { useScroll } from "motion/react";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 interface TimelineEntry {
 	title: string;
@@ -8,25 +6,9 @@ interface TimelineEntry {
 }
 
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
-	const ref = useRef<HTMLDivElement>(null);
-	const containerRef = useRef<HTMLDivElement>(null);
-	const [height, setHeight] = useState(0);
-
-	useEffect(() => {
-		if (ref.current) {
-			const rect = ref.current.getBoundingClientRect();
-			setHeight(rect.height);
-		}
-	}, [ref]);
-
-	const { scrollYProgress } = useScroll({
-		target: containerRef,
-		offset: ["start 10%", "end 50%"],
-	});
-
 	return (
-		<div className="w-full bg-none font-sans md:px-10" ref={containerRef}>
-			<div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+		<div className="w-full bg-none font-sans md:px-10">
+			<div className="relative max-w-7xl mx-auto pb-20">
 				{data.map((item, index) => (
 					<div key={index} className="flex justify-start pt-10 md:pt-40 md:gap-10">
 						<div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
@@ -43,19 +25,6 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 						</div>
 					</div>
 				))}
-				{/* <div
-					style={{
-						height: height + "px",
-					}}
-					className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] ">
-					<motion.div
-						style={{
-							height: heightTransform,
-							opacity: opacityTransform,
-						}}
-						className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
-					/>
-				</div> */}
 			</div>
 		</div>
 	);
